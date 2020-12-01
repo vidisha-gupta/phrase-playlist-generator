f32bf1c178b6733c3d5ac497fa021d344c5dd0d3
vidisha-gupta <52580831+vidisha-gupta@users.noreply.github.com>
2020-11-30 22:03:40 -0800

    added html skeleton

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-import spotipy
+import spotipy, os
 from dotenv import load_dotenv
-import os
 from spotipy.oauth2 import SpotifyOAuth
 
+# the data we are requesting from the user
 scope = "user-library-read user-read-private playlist-modify-public playlist-modify-private"
 
+# access secrets from .env file
 load_dotenv()
 SPOTIPY_CLIENT_ID = os.getenv("CLIENT_ID")
 SPOTIPY_CLIENT_SECRET= os.getenv("CLIENT_SECRET")
 SPOTIPY_REDIRECT_URI= os.getenv("REDIRECT_URI")
 
+# send request to Spotify
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id = SPOTIPY_CLIENT_ID, client_secret = SPOTIPY_CLIENT_SECRET,
                                                     redirect_uri = SPOTIPY_REDIRECT_URI, scope = scope))
 
