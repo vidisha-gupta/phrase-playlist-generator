9e3412c115856df67bcea2312cf9d6ea7f51a168
Alan F <alannfann@gmail.com>
2020-12-01 17:32:20 -0800

    Implemented adding track to playlist by search

--- auth.py
+++ auth.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
 
 # the data we are requesting from the user
-scope = "user-library-read user-read-private playlist-modify-public playlist-modify-private"
+scope = "user-read-private playlist-modify-public playlist-modify-private"
 
 # access secrets from .env file
 load_dotenv()
@@ -24,6 +24,13 @@ def add(user, playlist_id, tracks):
     print()
     spotify.user_playlist_add_tracks(user=spotify.me()['id'], playlist_id=playlist_id, tracks=tracks, position=None)
 
-coolPlaylist = createEmptyPlaylist()
-songzzz = ["spotify:track:7GhIk7Il098yCjg4BQjzvb"]
-add(spotify.me()['id'], coolPlaylist, songzzz)
+playlist = createEmptyPlaylist()
+
+name = "baby shark"
+results = spotify.search(q='track:' + name, type='track')
+if len(results['tracks']) > 0:
+    topTrack = results['tracks']['items'][0]['uri']
+
+songs = [topTrack]
+
+add(spotify.me()['id'], playlist, songs)
