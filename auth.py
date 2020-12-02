dcc5991f219616427430aab88a37bef74ef1f646
Alan F <alannfann@gmail.com>
2020-12-01 17:40:03 -0800

    Consolidated adding track to playlist by search into one method

--- auth.py
+++ auth.py
@@ -20,17 +20,12 @@ def createEmptyPlaylist():
     newPlaylist = spotify.user_playlist_create(user=spotify.me()['id'], name="i loveeeee akibabi hehehhe", public=True, collaborative=False, description="test ooh ahh abolish man, return to monke")
     return newPlaylist["id"]
 
-def add(user, playlist_id, tracks):
-    print()
+def add(user, playlist_id, name):
+    results = spotify.search(q='track:' + name, type='track')
+    if len(results['tracks']) > 0:
+        topTrack = results['tracks']['items'][0]['uri']
+    tracks = [topTrack]
     spotify.user_playlist_add_tracks(user=spotify.me()['id'], playlist_id=playlist_id, tracks=tracks, position=None)
 
 playlist = createEmptyPlaylist()
-
-name = "baby shark"
-results = spotify.search(q='track:' + name, type='track')
-if len(results['tracks']) > 0:
-    topTrack = results['tracks']['items'][0]['uri']
-
-songs = [topTrack]
-
-add(spotify.me()['id'], playlist, songs)
+add(spotify.me()['id'], playlist, "baby shark")
